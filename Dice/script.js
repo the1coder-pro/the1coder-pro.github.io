6b0baf1722442b021004c0769401baffe48cd6a5
MutawaPro <42838648+MutawaPro@users.noreply.github.com>
2018-10-31 19:57:25 +0300

    Update script.js

--- Dice/script.js
+++ Dice/script.js
@@ -1,3 +1,11 @@
 function Roll(number){
     document.getElementById('Dice_number').innerHTML = Math.floor(Math.random() * number) + 1;
 }
+
+window.addEventListener("keydown", checkKeyPressed, false);
+
+function checkKeyPressed(e) {
+   if (e.keyCode == "82") {
+       Roll(6)
+   }
+}
