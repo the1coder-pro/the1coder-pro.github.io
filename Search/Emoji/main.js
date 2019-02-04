c0ea712a312ce67cb5c5c8f5ee3f02f18a08199b
MutawaPro <42838648+MutawaPro@users.noreply.github.com>
2019-02-04 19:23:49 +0300

    Emoji

---
+++ Search/Emoji/main.js
@@ -0,0 +1,47 @@
+function Searcher() {
+    var search = document.getElementById("search").value;
+    if (search == "Rob"){
+        window.location.href = "Rob/index.html";
+    }else if (search == "wordbeater") {
+        window.location.href = "Wordbeater/index.html"
+    }else if (search == "WolfG"){
+        window.location.href = "https://showme-gh-pages.github.io/wolf3d/"
+    }else if (search == "CFE"){
+        window.location.href = "computer.html"
+    }else if (search == "OS"){
+        window.location.href = "DoorOS.html"
+    }else if (search == "Lamp"){
+        window.location.href = "Lamp/index.html"
+    }else if (search == "Dice"){
+        window.location.href = "Dice/index.html"
+    }else if (search == "CFE2"){
+        window.location.href = "Computer2.html"
+    }else if (search == "GL"){
+        window.location.href = "GameLess.html"
+    }else if (search == "Shelly"){
+        window.location.href = "Shelly/index.html"
+    }else if (search == "RPS"){
+        window.location.href = "RPS/index.html"
+    }else if (search == "Menu"){
+        window.location.href = "Menu/index.html"
+    }else if (search == "Registery"){
+        window.location.href = "Registery/index.html"
+    }else if (search == "Animal"){
+        window.location.href = "https://www.google.com/search?q=animal+sounds"
+    }else if (search == "About"){
+        window.location.href = "about/index.html"
+    }else if (search == "WhoUs"){
+        window.location.href = "Who Us/index.html"
+    }else if (search == "Coffee" && "coffee"){
+        window.location.href = "Search/Coffee.html"
+    }else{
+    window.location.href = "https://www.google.com.sa/search?q=" + search;
+    }
+ }
+
+window.addEventListener("keydown", checkKeyPressed, false);
+function checkKeyPressed(e) {
+   if (e.keyCode == "13") {
+       Searcher()
+   }
+}
