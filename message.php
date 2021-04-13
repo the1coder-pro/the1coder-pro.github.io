634a35b5a89e6609246c3c163cad3d1aacd4298e
MutawaPro <42838648+the1coder-pro@users.noreply.github.com>
2021-04-14 01:20:52 +0300

    Update message.php

--- message.php
+++ message.php
@@ -3,9 +3,9 @@ $app_name = $_POST[“app”];
 $sender   = $_POST[“sender”];
 $message  = $_POST[“message”];
 
-if ($app_name == “WhatsApp”) {
-  if ($sender == “Bob”) {
-     if ($message == “order status”) {
+if ($app_name == “WhatsAuto”) {
+  if ($sender == “WhatsAuto”) {
+     if ($message == “Test message”) {
        $dice = rand(1,6);
          $reply = array(“reply” => “what i got is $dice”);
         echo json_encode($reply);
