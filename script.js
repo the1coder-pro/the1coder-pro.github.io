982ea1cb7599422872869f3f655b3130a5aa9615
Mhmd Ahmd <42838648+the1coder-pro@users.noreply.github.com>
2024-10-01 15:10:20 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,47 +1,46 @@
 function Searcher() {
     var search = document.getElementById("search").value;
-    var search = "=" + search;
-    if (search == "Rob"){
+    if (search == "=Rob"){
         window.location.href = "Rob/index.html";
-    }else if (search == "wordbeater") {
+    }else if (search == "=wordbeater") {
         window.location.href = "Wordbeater/index.html"
-    }else if (search == "WolfG"){
+    }else if (search == "=WolfG"){
         window.location.href = "https://showme-gh-pages.github.io/wolf3d/"
-    }else if (search == "CFE"){
+    }else if (search == "=CFE"){
         window.location.href = "computer.html"
-    }else if (search == "OS"){
+    }else if (search == "=OS"){
         window.location.href = "DoorOS.html"
-    }else if (search == "Lamp"){
+    }else if (search == "=Lamp"){
         window.location.href = "Lamp/index.html"
-    }else if (search == "Dice"){
+    }else if (search == "=Dice"){
         window.location.href = "Dice/index.html"
-    }else if (search == "CFE2"){
+    }else if (search == "=CFE2"){
         window.location.href = "Computer2.html"
-    }else if (search == "GL"){
+    }else if (search == "=GL"){
         window.location.href = "GameLess.html"
-    }else if (search == "Shelly"){
+    }else if (search == "=Shelly"){
         window.location.href = "Shelly/index.html"
-    }else if (search == "RPS"){
+    }else if (search == "=RPS"){
         window.location.href = "RPS/index.html"
-    }else if (search == "Menu"){
+    }else if (search == "=Menu"){
         window.location.href = "Menu/index.html"
-    }else if (search == "Registery"){
+    }else if (search == "=Registery"){
         window.location.href = "Registery/index.html"
-    }else if (search == "Animal"){
+    }else if (search == "=Animal"){
         window.location.href = "https://www.google.com/search?q=animal+sounds"
-    }else if (search == "About"){
+    }else if (search == "=About"){
         window.location.href = "about/index.html"
-    }else if (search == "WhoUs"){
+    }else if (search == "=WhoUs"){
         window.location.href = "Who Us/index.html"
-    }else if (search == "Coffee" && "coffee"){
+    }else if (search == "=Coffee" && "coffee"){
         window.location.href = "Search/Coffee.html"
-    }else if (search == "Emoji"){
+    }else if (search == "=Emoji"){
         window.location.href = "Search/Emoji/Emoji.html"
-    }else if (search == "Emoji"){
+    }else if (search == "=Emoji"){
         window.location.href = "Search/Emoji/Emoji.html"
-    }else if (search == "Pay"){
+    }else if (search == "=Pay"){
         window.location.href = "Pay/index.html"
-    }if (search == "COVID"){
+    }if (search == "=COVID"){
         window.location.href = "Covid-19/main.html"
     }else{
     window.location.href = "https://www.google.com.sa/search?q=" + search;
